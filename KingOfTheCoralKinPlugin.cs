b8fd93bb59b83103b22d5ab7b300c3f22914ef17
Evryon <fireden70@gmail.com>
2025-10-17 13:36:08 +0200

    Added teleport to boss and config to enable that

--- KingOfTheCoralKinPlugin.cs
+++ KingOfTheCoralKinPlugin.cs
@@ -16,32 +16,50 @@ public partial class KingOfTheCoralKinPlugin : BaseUnityPlugin
     private static readonly ManualLogSource logger = BepInEx.Logging.Logger.CreateLogSource("King Of The Coral Kin");
     public static void log(string msg) => logger.LogInfo(msg);
     
-    public static bool inCoralMemory = false; //? Bool that checks wether we are in the Memory_Coral_Tower, changes on scene load
+    public static bool inCoralMemory; //? Bool that checks wether we are in the Memory_Coral_Tower, changes on scene load
+    private static bool teleportToBoss;
     private static readonly Vector3 CORAL_MEMORY_BOSS = new (
-        x: 60.08588f,
-        y: 550.6035f,
+        x: 55,
+        y: 510,
         z: 0.004f
     );
+
+    private static HeroController hornet;
     private void Awake()
     {
-        Harmony.CreateAndPatchAll(typeof(KingOfTheCoralKinPlugin));
+        teleportToBoss = Config.Bind(
+            "King Of The Coral Kin",
+            "TeleportOnSuperDash",
+            true,
+            "If true, the silksoar will take hornet directly up to the boss, skipping everything else"
+        ).Value;
         StartCoroutine(Language_Get_Patch.WaitAndPatch());
-        SceneManager.sceneLoaded += (Scene scene, LoadSceneMode mode) =>
-        {
-            inCoralMemory = scene.name == "Memory_Coral_Tower";
-        };
+        Harmony.CreateAndPatchAll(typeof(KingOfTheCoralKinPlugin));
+        SceneManager.sceneLoaded += sceneLoadSetup;
         Logger.LogInfo($"Plugin {Name} ({Id}) has loaded!");
     }
-
+    private static void sceneLoadSetup(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name != "Memory_Coral_Tower")
+        {
+            inCoralMemory = false;
+            return;
+        }
+        inCoralMemory = true;
+        hornet = HeroController.instance;
+    }
     [HarmonyPostfix]
     [HarmonyPatch(typeof(PlayMakerFSM), "OnEnable")]
     private static void OnFsmEnabled(PlayMakerFSM __instance)
     {
         
     }
-
     private void FixedUpdate()
     {
+        if (!inCoralMemory) return;
+        
+        //TODO: Make this less scuffed
+        if (hornet.cState.superDashing && teleportToBoss && hornet.transform.position.y is > 40 and < 50) hornet.transform.position = CORAL_MEMORY_BOSS;
     }
 }
 [HarmonyPatch(typeof(Language), "Get")]
